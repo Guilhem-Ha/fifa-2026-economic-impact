a369ba7fbd833b0a32d329652bb692e853b900aa
Guilhem Hasselin <guilhem.hasselin.1@ens.etsmtl.ca>
2026-03-27 16:08:43 -0400

    Create analysis script for FIFA 2026 Economic Impact

---
+++ src/01_data_collection_eda.py
@@ -0,0 +1,20 @@
+# FIFA 2026 Economic Impact Analysis
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load your dataset
+# df = pd.read_csv('data.csv')
+
+# Preliminary Data Exploration
+# df.info()
+# df.describe()
+
+# Data Cleaning
+# df.dropna(inplace=True)
+
+# Exploratory Data Analysis
+# sns.countplot(x='column_name', data=df)
+# plt.show()
